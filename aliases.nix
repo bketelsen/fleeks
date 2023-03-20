a8bd0e8db794d68d78a7803cc402e756901738d8
Brian Ketelsen <bketelsen@gmail.com>
2023-03-20 13:14:09 -0400

    fleek: update configs

--- aliases.nix
+++ aliases.nix
@@ -1,9 +1,13 @@
 { pkgs, misc, ... }: {
    home.shellAliases = {
     
+    bluefin = "cd ~/projects/ublue/bluefin";
+    
     cdfleek = "cd ~/projects/ublue/fleek";
     
     fleeks = "cd ~/.config/home-manager";
+    
+    ublue = "cd ~/projects/ublue";
     
   };
 
