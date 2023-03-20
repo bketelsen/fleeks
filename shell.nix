d444b96e5b2a21f256e2f52bb02876461108d38d
Brian Ketelsen <bketelsen@gmail.com>
2023-03-20 12:33:51 -0400

    fix: shell

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 
     # bash
     
-    programs.bash.profileExtra = ". /.nix-profile/etc/profile.d/nix.sh";
+    programs.bash.profileExtra = ". ~/.nix-profile/etc/profile.d/nix.sh";
     programs.bash.enableCompletion = true;
     programs.bash.enableVteIntegration = true;
     programs.bash.enable = true;
