d576aabf68848cb09cf97816e3de7a31ff38a593
bketelsen@gmail.com <Brian Ketelsen>
2023-03-21 09:57:15 -0400

    fleek: update configs

---
+++ f84d89911e5d.ant.amazon.com/f84d89911e5d.ant.amazon.com.nix
@@ -0,0 +1,7 @@
+{ pkgs, misc, ... }: {
+    home.username = "brianjk";
+    home.homeDirectory = "/Users/brianjk";
+    programs.git.enable = true;
+    programs.git.userEmail = "Brian Ketelsen";
+    programs.git.userName = "bketelsen@gmail.com";
+}
--- flake.nix
+++ flake.nix
@@ -63,6 +63,22 @@
         ];
       };
       
+      "brianjk@f84d89911e5d.ant.amazon.com" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [ 
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./f84d89911e5d.ant.amazon.com/f84d89911e5d.ant.amazon.com.nix
+          ./f84d89911e5d.ant.amazon.com/user.nix
+        ];
+      };
+      
     };
   };
 }
