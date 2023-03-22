f2520d845e353d621e9897bfbd2e4d86993b9fc2
Brian Ketelsen <bketelsen@gmail.com>
2023-03-22 12:38:32 -0400

    fleek: commit

--- chapterhouse/chapterhouse.nix
+++ chapterhouse/chapterhouse.nix
@@ -1,7 +1,7 @@
 { pkgs, misc, ... }: {
     home.username = "bjk";
     home.homeDirectory = "/Users/bjk";
-    programs.git.enable = true;
+    #programs.git.enable = true;
     programs.git.userEmail = "bketelsen@gmail.com";
     programs.git.userName = "Brian Ketelsen";
 }
--- ghanima/ghanima.nix
+++ ghanima/ghanima.nix
@@ -1,7 +1,7 @@
 { pkgs, misc, ... }: {
     home.username = "bjk";
     home.homeDirectory = "/home/bjk";
-    programs.git.enable = true;
+    #programs.git.enable = true;
     programs.git.userEmail = "bketelsen@gmail.com";
     programs.git.userName = "Brian Ketelsen";
 }
