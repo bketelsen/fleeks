3628d633f4da1c7389c7267be617776a2985c969
Brian Ketelsen <bketelsen@gmail.com>
2023-03-27 11:42:29 -0400

    fleek: commit

--- programs.nix
+++ programs.nix
@@ -2,11 +2,15 @@
   # packages are just installed (no configuration applied)
   # programs are installed and configuration applied to dotfiles
   # add your personalized program configuration in ./user.nix
-    programs.dircolors.enable = true;# low bling
+    programs.dircolors.enable = true;
+    # low bling
     programs.starship.enable = true;
     # default bling
-    programs.direnv.enable = true;# high bling
+    programs.direnv.enable = true;
+    # high bling
     programs.exa.enable = true;
     programs.bat.enable = true;
     programs.atuin.enable = true;
-    programs.zoxide.enable = true;}
+    programs.zoxide.enable = true;
+    
+}
