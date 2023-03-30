4520c007493260422c7d78d32d799e80f1b0829b
Brian Ketelsen <bketelsen@gmail.com>
2023-03-29 21:40:46 -0400

    fleek: commit

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@
 # bat
  
   programs.bat.config = {
-theme = &#34;TwoDark&#34;;
+theme = \&#34;TwoDark\&#34;;
 }
 ;
    
@@ -16,8 +16,8 @@ theme = &#34;TwoDark&#34;;
   programs.exa.enableAliases = true;
    
   programs.exa.extraOptions = [
-&#34;--group-directories-first&#34; 
-&#34;--header&#34;
+\&#34;--group-directories-first\&#34; 
+\&#34;--header\&#34;
 ];;
    
 # starship
