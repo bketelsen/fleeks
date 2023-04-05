0ba523a2f0ae7d866cba227d883ed1788fdca1b2
bketelsen@gmail.com <Brian Ketelsen>
2023-04-04 21:25:44 -0400

    something

--- shell.nix
+++ shell.nix
@@ -1,28 +1,28 @@
 { pkgs, misc, ... }: {
- 
+
 # atuin
- 
+
 # bat
- 
+
   programs.bat.config = {
   theme = "TwoDark";
 };
-   
+
 # direnv
- 
+
 # exa
- 
+
   programs.exa.enableAliases = true;
-   
+
   programs.exa.extraOptions = [
    "--group-directories-first"
    "--header"
 ];
-   
+
 # starship
 # zsh
   programs.zsh.profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh";
   programs.zsh.enableCompletion = true;
   programs.zsh.enable = true;
-  programs.zsh.initExtra = "source <(fleek completion zsh); compdef _fleek fleek";
+  #programs.zsh.initExtra = "source <(fleek completion zsh); compdef _fleek fleek";
 }
